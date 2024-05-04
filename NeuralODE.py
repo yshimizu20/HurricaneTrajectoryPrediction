582c1b210ca888b425010510c54e6f6f3f450075
Yukoh Shimizu <yukoh.shimizu@yale.edu>
2024-05-04 19:43:25 -0400

    modularized NeuralODE.py

--- NeuralODE.py
+++ NeuralODE.py
@@ -26,77 +26,77 @@ JSON_DIR = SAVE_DIR / "HURDAT2_final.json"
 # Set device
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-# Load data and print first 5 elements
-"""Format:
-First Column: longitude
-Second: Latitude
-Third: Wind Speed
-Fourth: Presure
-Afterwards: Image Embeddings
-"""
-storm_data = torch.load(VECTOR_REPS_DIR)
+def prepare_data():
+    """
+    Function to prepare data for training and testing
+    Returns: training and test data loaders
+    """
+    # Load data and print first 5 elements
+    """Format:
+    First Column: longitude
+    Second: Latitude
+    Third: Wind Speed
+    Fourth: Presure
+    Afterwards: Image Embeddings
+    """
+    storm_data = torch.load(VECTOR_REPS_DIR)
 
-# Load JSON file
-with open(JSON_DIR, "r") as f:
-    storm_name_to_id = json.load(f)
+    # Load JSON file
+    with open(JSON_DIR, "r") as f:
+        storm_name_to_id = json.load(f)
 
-# Investigate data and model
-# print(storm_data[:5])
-# print(model)
+    all_ids = list(storm_name_to_id.values())
 
-# Split data into training and test sets
-data_size = len(storm_name_to_id)
-training_size = int(0.8 * data_size)
-test_size = data_size - training_size
+    # Split data into training and test sets
+    data_size = len(storm_name_to_id)
+    training_size = int(0.8 * data_size)
 
-# randomly assign ids to training and test data
-training_ids = torch.randperm(data_size)[:training_size]
-test_ids = torch.randperm(data_size)[training_size:]
+    # randomly assign ids to training and test data
+    perm = torch.randperm(data_size)
+    training_id_idx = perm[:training_size]
+    test_id_idx = perm[training_size:]
 
-training_data = defaultdict(lambda: {"X": [], "y": []})
-test_data = defaultdict(lambda: {"X": [], "y": []})
+    training_ids = [all_ids[i] for i in training_id_idx]
+    test_ids = [all_ids[i] for i in test_id_idx]
 
-# walk through the data and print the 1st element
-for vec in storm_data:
-    storm_id = int(vec[0])
-    if storm_id in training_ids:
-        training_data[storm_id]["X"].append(torch.tensor(vec[4:]))
-        training_data[storm_id]["y"].append(torch.tensor(vec[2:4]))
-    else:
-        test_data[storm_id]["X"].append(torch.tensor(vec[4:]))
-        test_data[storm_id]["y"].append(torch.tensor(vec[2:4]))
+    training_data = defaultdict(lambda: {"X": [], "y": []})
+    test_data = defaultdict(lambda: {"X": [], "y": []})
 
-# stack the tensors
-training_tensors = list(
-    {"X": torch.stack(data["X"]), "y": torch.stack(data["y"])}
-    for data in training_data.values()
-)
-test_tensors = list(
-    {"X": torch.stack(data["X"]), "y": torch.stack(data["y"])}
-    for data in test_data.values()
-)
+    # walk through the data and print the 1st element
+    for vec in storm_data:
+        storm_id = int(vec[0])
+        if storm_id in training_ids:
+            training_data[storm_id]["X"].append(torch.tensor(vec[4:]))
+            training_data[storm_id]["y"].append(torch.tensor(vec[2:4]))
+        elif storm_id in test_ids:
+            test_data[storm_id]["X"].append(torch.tensor(vec[4:]))
+            test_data[storm_id]["y"].append(torch.tensor(vec[2:4]))
+        else:
+            raise ZeroDivisionError("Invalid ID")
 
-# make sure all tensors have dimension of 34 and 2
-for d in training_tensors:
-    assert d["X"].shape[1] == 34
-    assert d["y"].shape[1] == 2
+    # stack the tensors
+    training_tensors = list(
+        {"X": torch.stack(data["X"]), "y": torch.stack(data["y"])}
+        for data in training_data.values()
+    )
+    test_tensors = list(
+        {"X": torch.stack(data["X"]), "y": torch.stack(data["y"])}
+        for data in test_data.values()
+    )
 
-for d in test_tensors:
-    assert d["X"].shape[1] == 34
-    assert d["y"].shape[1] == 2
+    # make sure all tensors have dimension of 34 and 2
+    for d in training_tensors:
+        assert d["X"].shape[1] == 34
+        assert d["y"].shape[1] == 2
 
-train_loader = CustomDataLoader(training_tensors)
-test_loader = CustomDataLoader(test_tensors, shuffle=False)
+    for d in test_tensors:
+        assert d["X"].shape[1] == 34
+        assert d["y"].shape[1] == 2
 
-# Define NDE Model
-"""
-Input is size 36: long, lat, wind, pressure, 
-and 32 vectorized image features
-Hidden features size: 10
-Output features sizel: 2 (long and lat)
-"""
-model = NeuralODE(FlowNet(36), sensitivity="adjoint", solver="dopri5").to(device)
-optimizer = optim.Adam(model.parameters(), lr=0.01)
+    train_loader = CustomDataLoader(training_tensors)
+    test_loader = CustomDataLoader(test_tensors, shuffle=False)
+
+    return train_loader, test_loader
 
 """
 Hurricanes last 10 days. That's 40 6-hour periods.
@@ -148,7 +148,20 @@ def test(model, device, test_loader):
             total_loss += loss.item()
     print(f"Test Loss: {total_loss / len(test_loader)}")
 
+if __name__ == "__main__":
+    # Define NDE Model
+    """
+    Input is size 36: long, lat, wind, pressure, 
+    and 32 vectorized image features
+    Hidden features size: 10
+    Output features sizel: 2 (long and lat)
+    """
+    model = NeuralODE(FlowNet(36), sensitivity="adjoint", solver="dopri5").to(device)
+    optimizer = optim.Adam(model.parameters(), lr=0.01)
+    
+    # Prepare data
+    train_loader, test_loader = prepare_data()
 
-# Training and testing the model
-train(model, device, train_loader, optimizer, num_epochs=100)
-test(model, device, test_loader)
+    # Training and testing the model
+    train(model, device, train_loader, optimizer, num_epochs=100)
+    test(model, device, test_loader)
