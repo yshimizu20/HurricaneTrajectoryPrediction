4adefd8bce1a9cdfcf4398a370d50d07b229396a
Yukoh Shimizu <cpsc452_ys669@r206u22n01.mccleary.ycrc.yale.edu>
2024-05-05 23:04:39 -0400

    training now running on clusters

--- test_ode_combined.py
+++ test_ode_combined.py
@@ -22,14 +22,17 @@ SOLVER_LIST = [
     # "AsynchronousLeapfrog",
 ]
 
+# if "log/" does not exist, create it
+if not os.path.exists("logs"):
+    os.makedirs("logs")
+if not os.path.exists("logs/combined"):
+    os.makedirs("logs/combined")
+
 for sensitivity in sensitivity_list:
     for forward_solver in SOLVER_LIST:
         for backward_solver in SOLVER_LIST:
-            # if "log/" does not exist, create it
-            if not os.path.exists("logs"):
-                os.makedirs("logs")
-            if not os.path.exists("logs/combined"):
-                os.makedirs("logs/combined")
+            if forward_solver == backward_solver:
+                continue
 
             log_path = f"logs/combined/{sensitivity}_{forward_solver}_{backward_solver}.log"
 
@@ -47,7 +50,7 @@ for sensitivity in sensitivity_list:
             optimizer = optim.Adam(model.parameters(), lr=0.01)
 
             # Train the model
-            train(model, device, training_loader, test_loader, optimizer, num_epochs=1, log_path=log_path)
+            train(model, device, training_loader, test_loader, optimizer, num_epochs=500, log_path=log_path)
 
             # Test the model
             test(model, device, test_loader, log_path=log_path)
